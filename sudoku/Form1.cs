465d8418a8fb2629520fa59e34c55e83fa8446a5
Sun Zhao <zixiaojindao@gmail.com>
2012-12-04 18:15:00 +0800

    init

---
+++ sudoku/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace sudoku
+{
+    public partial class Form1 : Form
+    {
+        private const int m_formWidth = 420;
+        private const int m_formHeight = 300;
+        private const int m_textBoxWidth = 26;
+        private const int m_textBoxHeight = 20;
+        private const int m_soDoKuSize = 9;
+        private const int m_topSpace = 20;
+        private const int m_leftSpace = 50;
+        private const int m_splitSpace = 5;
+        private TextBox[][] m_allTextBoxes;
+        private int[][] m_data;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.Text = "SoDoKu";
+            this.ClientSize = new Size(m_formWidth, m_formHeight);
+            InitizlizeTextBoxes();
+            m_data = new int[m_soDoKuSize][];
+            for (int i = 0; i < m_data.Length; ++i)
+            {
+                m_data[i] = new int[m_soDoKuSize];
+            }
+        }
+
+        private void InitizlizeTextBoxes()
+        {
+            m_allTextBoxes = new TextBox[m_soDoKuSize][];
+            for (int i = 0; i < m_allTextBoxes.Length; ++i)
+            {
+                m_allTextBoxes[i] = new TextBox[m_soDoKuSize];
+            }
+            for (int i = 0; i < m_allTextBoxes.Length; ++i)
+            {
+                for (int j = 0; j < m_allTextBoxes[i].Length; ++j)
+                {
+                    TextBox tb = new TextBox();
+                    int x = m_leftSpace + (m_splitSpace + m_textBoxWidth) * j;
+                    int y = m_textBoxWidth + (m_splitSpace + m_textBoxHeight) * i;
+                    tb.Location = new System.Drawing.Point(x, y);
+                    tb.Size = new System.Drawing.Size(m_textBoxWidth, m_textBoxHeight);
+                    tb.TextAlign = HorizontalAlignment.Center;
+                    this.Controls.Add(tb);
+                    m_allTextBoxes[i][j] = tb;
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < m_allTextBoxes.Length; ++i)
+            {
+                for (int j = 0; j < m_allTextBoxes[i].Length; ++j)
+                {
+                    m_allTextBoxes[i][j].Text = "";
+                    m_allTextBoxes[i][j].BackColor = Color.White;
+                }
+            }
+        }
+
+        private void GetData()
+        {
+            for (int i = 0; i < m_allTextBoxes.Length; ++i)
+            {
+                for (int j = 0; j < m_allTextBoxes[i].Length; ++j)
+                {
+                    try
+                    {
+                        if (m_allTextBoxes[i][j].Text.Trim() == "")
+                        {
+                            m_data[i][j] = 0;
+                        }
+                        else
+                        {
+                            m_data[i][j] = int.Parse(m_allTextBoxes[i][j].Text.Trim());
+                            m_allTextBoxes[i][j].BackColor = Color.Green;
+                        } 
+                    }
+                    catch (Exception e)
+                    {
+                        MessageBox.Show("Row " + (i + 1) + ": Col " + (j + 1) + "format uncorret!");
+                    }
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            GetData();
+            SoDoKU sdk = new SoDoKU(m_data);
+            int[][] rdata = sdk.ShowAnswer();
+            if (rdata != null)
+            {
+                for (int i = 0; i < m_allTextBoxes.Length; ++i)
+                {
+                    for (int j = 0; j < m_allTextBoxes[i].Length; ++j)
+                    {
+                        m_allTextBoxes[i][j].Text = rdata[i][j].ToString();
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("can not find a anwer!");
+            }
+        }
+    }
+}
---
+++ sudoku/SoDoKU.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace sudoku
+{
+    class SoDoKU
+    {
+        private int[][] m_input;
+        private int[][] m_data;
+        private int m_size = 9;
+        private int m_blockSize = 3;
+        private int m_rowBlocks;  
+        private bool[][] m_rowCheck;
+        private bool[][] m_colCheck;
+        private bool[][] m_blockCheck;
+
+        public SoDoKU(int[][] input)
+        {
+            m_input = input;
+            int size = m_size;
+            m_rowBlocks = m_size / m_blockSize;
+            m_rowCheck = new bool[size][];
+            m_colCheck = new bool[size][];
+            m_blockCheck = new bool[size][];
+            m_data = new int[size][];
+            for (int i = 0; i < size; ++i)
+            {
+                m_rowCheck[i] = new bool[size];
+                m_colCheck[i] = new bool[size];
+                m_blockCheck[i] = new bool[size];
+                m_data[i] = new int[size];
+                for (int j = 0; j < size; ++j)
+                {
+                    m_rowCheck[i][j] = false;
+                    m_colCheck[i][j] = false;
+                    m_blockCheck[i][j] = false;
+                    m_data[i][j] = 0;
+                }
+                
+            }
+            if (Certain() == true)
+            {
+                for (int i = 0; i < size; ++i)
+                {
+                    for (int j = 0; j < size; ++j)
+                    {
+                        if (m_input[i][j] != 0)
+                        {
+                            m_rowCheck[i][m_input[i][j] - 1] = true;
+                            m_colCheck[j][m_input[i][j] - 1] = true;
+                            int bi = i / m_blockSize;
+                            int bj = j / m_blockSize;
+                            int bindex = bi * m_rowBlocks + bj;
+                            m_blockCheck[bindex][m_input[i][j] - 1] = true;
+                        }
+                    }
+                }
+            }
+        }
+
+        private bool Calculate(int c)
+        {
+            if (c == m_size * m_size)
+                return true;
+
+            int i = c / m_size;
+            int j = c % m_size;
+            int bi = i / m_blockSize;
+            int bj = j / m_blockSize;
+            int bindex = bi * m_rowBlocks + bj;
+            if(m_input[i][j] != 0)
+            {
+                m_data[i][j] = m_input[i][j];
+                return Calculate(c + 1);
+            }
+            for (int k = 1; k <= m_size; ++k)
+            {
+                if (m_rowCheck[i][k - 1] == false && 
+                    m_colCheck[j][k - 1] == false && 
+                    m_blockCheck[bindex][k - 1] == false)
+                {
+                    m_rowCheck[i][k - 1] = true;
+                    m_colCheck[j][k - 1] = true;
+                    m_blockCheck[bindex][k - 1] = true;
+                    m_data[i][j] = k;
+                    if (Calculate(c + 1) == true)
+                    {
+                        return true;
+                    }
+                    m_rowCheck[i][k - 1] = false;
+                    m_colCheck[j][k - 1] = false;
+                    m_blockCheck[bindex][k - 1] = false;
+                    m_data[i][j] = 0;
+                }
+            }
+            return false;
+        }
+
+        private bool Certain()
+        {
+            if (m_data.Length < m_size)
+            {
+                return false;
+            }
+            for (int i = 0; i < m_size; ++i)
+            {
+                if (m_data[i].Length < m_size)
+                {
+                    return false;
+                }
+            }
+            for (int i = 0; i < m_size; ++i)
+            {
+                bool[] check = new bool[m_size];
+                for (int j = 0; j < m_size; ++j)
+                {
+                    if (m_input[i][j] != 0)
+                    {
+                        if (m_input[i][j] > m_size || m_input[i][j] < 1)
+                        {
+                            return false;
+                        }
+
+                        if (check[m_input[i][j] - 1] == true)
+                        {
+                            return false;
+                        }
+                        else
+                        {
+                            check[m_input[i][j] - 1] = true;
+                        }
+                    }
+                }
+            }
+
+            for (int j = 0; j < m_size; ++j)
+            {
+                bool[] check = new bool[m_size];
+                for (int i = 0; i < m_size; ++i)
+                {
+                    if (m_input[i][j] != 0)
+                    {
+                        if (m_input[i][j] > m_size || m_input[i][j] < 1)
+                        {
+                            return false;
+                        }
+
+                        if (check[m_input[i][j] - 1] == true)
+                        {
+                            return false;
+                        }
+                        else
+                        {
+                            check[m_input[i][j] - 1] = true;
+                        }
+                    } 
+                }
+            }
+            return true;
+        }
+
+        public int[][] ShowAnswer()
+        {
+            if (Certain() == false)
+            {
+                return null;
+            }
+            if (Calculate(0) == true)
+            {
+                return m_data;
+            }
+            return null;
+        }
+    }
+}
